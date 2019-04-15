3dad4e2644b97af29ac4b5e218de676e52a8a6a5
dstanfor350 <dale_stanford350@hotmail.com>
2019-04-15 15:03:41 -0500

    Add .gitignore and .gitattributes.

---
+++ HiringTest/SeparateFilenames.cs
@@ -0,0 +1,54 @@
+﻿//
+// https://www.hackerrank.com/tests/7ah6dolcs15/questions/ahi7q7flqrk
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiringTest
+{
+    class SeparateFilenames
+    {
+        static void Main(string[] args)
+        {
+            //string filename = Console.ReadLine();
+            string filename = "names_list_00.txt";
+
+            FileStream twcf = new FileStream("c_names_list_00.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter twc = new StreamWriter(twcf);
+            FileStream twcppf = new FileStream("cpp_names_list_00.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter twcpp = new StreamWriter(twcppf);
+            FileStream twcsf = new FileStream("cs_names_list_00.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter twcs = new StreamWriter(twcsf);
+            try
+            {
+                using (StreamReader sr = new StreamReader(filename))
+                {
+                    string line, extension;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        extension = Path.GetExtension(line);
+                        if (extension == ".c")
+                            twc.WriteLine(line);
+                        else if (extension == ".cs")
+                            twcs.WriteLine(line);
+                        else if (extension == ".cpp")
+                            twcpp.WriteLine(line);
+
+                    }
+                }
+                twc.Close();
+                twcs.Close();
+                twcpp.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ NumberCharactersEscaped/NumberCharactersEscaped.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberCharactersEscaped
+{
+    class NumberCharactersEcs
+    {
+        static void Main(string[] args)
+        {
+            string expression = "##!r#po#";         // 0
+            //string expression = "#a!b#c";         // 1 
+            //string expression = "#ab!c#d#e!f#";   // 2
+            //string expression = "#ae#b#!c#d#e!f"  // 1, contains 1 unclosed segment
+            //string expression = "a!e#b#!c#d#e!f"; // 0, contains 1 uopened and 1 unclosed segment
+
+            bool inHash = false, chNextEsc = false, valid = false;
+            char[] lowerAlpha = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                                  'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z' };
+            int needsEscape = 0;
+
+            foreach (char ch in expression)
+            {
+                if (ch == '#' && !inHash) { inHash = true; valid = false;  continue; }
+                if (ch == '#' && inHash) { inHash = false; continue; }
+                if (ch == '!' && inHash) { chNextEsc = true; continue; }
+                if (chNextEsc && inHash && lowerAlpha.Contains(ch))
+                {
+                    needsEscape++;
+                    valid = true;
+                    chNextEsc = false;
+                }
+
+
+                //if (ch == '#' && !inHash) { inHash = true; continue; }
+                //if (ch == '#' && inHash) { inHash = false; continue; }
+                //if (ch == '!' && inHash) { chNextEsc = true; continue; }
+                //if (chNextEsc && inHash && lowerAlpha.Contains(ch)) {
+                //    //needsEscape++;
+                //    valid = true;
+                //    //chNext = false;
+                //}
+                //if (inHash && valid && chNextEsc)
+                //{
+                //    needsEscape++;
+                //    valid = false;
+                //}
+            }
+            if (inHash && valid)
+                needsEscape--;
+
+            Console.WriteLine(needsEscape);
+        }
+    }
+}
